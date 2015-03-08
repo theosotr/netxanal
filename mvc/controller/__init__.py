ae92f6df740ee12dd89025952f044ce921117895
Thodoris Sotiropoulos <theosotr@windowslive.com>
2015-03-08 17:27:16 +0200

    add package documentation

--- mvc/controller/__init__.py
+++ mvc/controller/__init__.py
@@ -1,1 +1,11 @@
-__author__ = 'theo'
+"""
+The application level of system.
+
+Contains the logical units of application which are the connectors of
+the presentation and data.
+
+Actually receives data from classes that are on the model level side, edits
+them in the required format and forwards to Servlets to sends them in client
+side to code to be represented to user.
+"""
+__author__ = 'Thodoris Sotiropoulos'
--- mvc/model/__init__.py
+++ mvc/model/__init__.py
@@ -1,1 +1,11 @@
-__author__ = 'theo'
+"""
+The model level of application.
+
+Contains modules which are the connectors between database and classes of
+application level of system. Contained classes are responsible for the execution
+of the right queries to access the required data.
+
+Then initializes classes or returns data to the classes of application level
+accordingly.
+"""
+__author__ = 'Thodoris Sotiropoulos'
