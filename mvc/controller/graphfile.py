c91d2d76fa0d3ff39ed680d29a3aa649e2a142d1
Thodoris Sotiropoulos <theosotr@windowslive.com>
2015-03-08 17:26:05 +0200

    add documentation

--- mvc/controller/graphfile.py
+++ mvc/controller/graphfile.py
@@ -1,3 +1,11 @@
-__author__ = 'theo'
+"""
+This module contains a dictionary which describes the graph object which being
+currently analyzed by a user.
+
+For example, if there are two users A and B which are simultaneously using system
+the keys of this dictionary will be A and B with values the graph object which
+each user is analyzing corresponding.
+"""
+__author__ = 'Thodoris Sotiropoulos'
 global graphfile
 graphfile = {}
